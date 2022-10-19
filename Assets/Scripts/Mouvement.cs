7b9c9d74377aae3adbd01b6bae71543975b9a1fb
PinGroLeon <nicolas.fauchay@gmail.com>
2022-10-19 15:35:46 +0200

    3.0 ça bouge
    déplacement 1.0

---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);  
+    }
+}
